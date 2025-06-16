7028a9d66f91b24da1ece3c87ffebbc4e42907fa
TheStackMaster <unijoy84@gmail.com>
2025-06-16 23:53:36 +0600

    Solving a minor issue

--- src/Pages/Books/MyBooks.jsx
+++ src/Pages/Books/MyBooks.jsx
@@ -47,12 +47,13 @@ const MyBooks = () => {
             confirmButtonText: "Yes, delete it!"
         }).then((result) => {
             if (result.isConfirmed) {
-                fetch(`${import.meta.env.VITE_SERVER_URL}/books/${id}`, {
-                    method: "DELETE"
+                axios.delete(`${import.meta.env.VITE_SERVER_URL}/books/${id}`, {
+                    headers: {
+                        Authorization: `Bearer ${user?.accessToken}`
+                    }
                 })
-                    .then(res => res.json())
-                    .then(data => {
-                        if (data.deletedCount) {
+                    .then(res => {
+                        if (res.data.deletedCount) {
                             const newBooks = books.filter(book => book._id !== id);
                             setBooks(newBooks);
 
@@ -63,7 +64,15 @@ const MyBooks = () => {
                                 timer: 1500
                             });
                         }
-                    });
+                    })
+                    .catch(err => {
+                        if (err?.status === 401 || err?.status === 403) {
+                            logOut()
+                                .then(() => {
+                                    console.log('Signed Out')
+                                })
+                        }
+                    })
             }
         });
     };
@@ -73,45 +82,45 @@ const MyBooks = () => {
 
             {
                 (loading) ? <Loading /> : books.length > 0 ? (<>
-                <ZoomInSection>
-                    <h1 className="text-4xl font-bold text-center text-[#242253] mb-6">My Reading Shelf</h1>
-                    <div className="overflow-x-auto rounded-box border border-base-content/5 bg-[#f4f3f3]">
-                        <table className="table">
-                            <thead className="bg-[#242253] text-white">
-                                <tr>
-                                    <th>No.</th>
-                                    <th></th>
-                                    <th>Title</th>
-                                    <th>Category</th>
-                                    <th>Reading Status</th>
-                                    <th>Total Upvote</th>
-                                    <th>Submitted By</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    books.map((book, index) => (
-                                        <tr className="text-[#242253] text-sm font-medium" key={book._id}>
-                                            <th>{index + 1}</th>
-                                            <td>
-                                                <img src={book.cover_photo} alt="" className="w-8 rounded" />
-                                            </td>
-                                            <td>{book.book_title}</td>
-                                            <td>{book.book_category}</td>
-                                            <td>{book.reading_status}</td>
-                                            <td>{book.upvote.length}</td>
-                                            <td>{book.name}</td>
-                                            <td className="flex gap-2">
-                                                <Link to={`/update-book/${book._id}`} className='btn bg-[#bfbdff] hover:bg-[#242253] transition-all text-[#242253] hover:text-white md:px-8'>Update</Link>
-                                                <button className='btn bg-[#242253] hover:bg-[#bfbdff] transition-all text-white hover:text-[#242253] md:px-8' onClick={() => handleDelete(book._id)}>Delete</button>
-                                            </td>
-                                        </tr>
-                                    ))
-                                }
-                            </tbody>
-                        </table>
-                    </div>
+                    <ZoomInSection>
+                        <h1 className="text-4xl font-bold text-center text-[#242253] mb-6">My Reading Shelf</h1>
+                        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-[#f4f3f3]">
+                            <table className="table">
+                                <thead className="bg-[#242253] text-white">
+                                    <tr>
+                                        <th>No.</th>
+                                        <th></th>
+                                        <th>Title</th>
+                                        <th>Category</th>
+                                        <th>Reading Status</th>
+                                        <th>Total Upvote</th>
+                                        <th>Submitted By</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        books.map((book, index) => (
+                                            <tr className="text-[#242253] text-sm font-medium" key={book._id}>
+                                                <th>{index + 1}</th>
+                                                <td>
+                                                    <img src={book.cover_photo} alt="" className="w-8 rounded" />
+                                                </td>
+                                                <td>{book.book_title}</td>
+                                                <td>{book.book_category}</td>
+                                                <td>{book.reading_status}</td>
+                                                <td>{book.upvote.length}</td>
+                                                <td>{book.name}</td>
+                                                <td className="flex gap-2">
+                                                    <Link to={`/update-book/${book._id}`} className='btn bg-[#bfbdff] hover:bg-[#242253] transition-all text-[#242253] hover:text-white md:px-8'>Update</Link>
+                                                    <button className='btn bg-[#242253] hover:bg-[#bfbdff] transition-all text-white hover:text-[#242253] md:px-8' onClick={() => handleDelete(book._id)}>Delete</button>
+                                                </td>
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
                     </ZoomInSection>
                 </>
                 ) : (
